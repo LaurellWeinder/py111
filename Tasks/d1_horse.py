b6fc4d7b155d1b4dff0b437f5e6540cd60faf8c6
Aleksandra Danilova <alex.l.univ@gmail.com>
2020-01-20 14:18:44 +0300

    Horse is done.

--- Tasks/d1_horse.py
+++ Tasks/d1_horse.py
@@ -8,16 +8,19 @@ def calculate_paths(shape: (int, int), point: (int, int)) -> int:
     :param point: desired point for horse
     :return: count of paths from (1, 1) to (point[0], point[1]) (numerating from 0, so (0, 0) - left bottom tile)
     """
-    l = np.zeros(shape)
-    m, n = point
-    print(l)
-    for i in range(len(l)):
-        for j in range(len(l), 0):
+    l = np.zeros(shape, dtype=np.int)
+    l[0][0] = 1
+    for i in range(shape[0]):
+        for j in range(shape[1]):
+            if (i + 1 < shape[0]) and (j + 2 < shape[1]):
+                l[i + 1, j + 2] += l[i, j] * 2
+            if (i + 1 < shape[0]) and (j - 2 >= 0):
+                l[i + 1, j - 2] += l[i, j] * 2
+            if (i + 2 < shape[0]) and (j - 1 >=0):
+                l[i + 2, j - 1] += 2 * l[i, j]
+            if (i + 2 < shape[0]) and (j + 1 < shape[1]):
+                l[i + 2, j + 1] += 2 * l[i, j]
+    return l[point[0]][point[1]]
 
-            l[i][j] += l[i - 2][j - 1] * 2 + l[i - 2][j + 1] * 2 \
-                    + l[i - 1][j - 2] * 2 + l[i + 1][j - 2] * 2
-            print(l[m][n])
 
 
-
-print(calculate_paths((8, 8), (7, 7)))
