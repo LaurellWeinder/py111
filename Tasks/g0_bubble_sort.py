b8670c758d382ac3a92899fe219f0605f8bc0c87
Aleksandra Danilova <alex.l.univ@gmail.com>
2020-01-20 14:22:01 +0300

    BubbleSort

--- Tasks/g0_bubble_sort.py
+++ Tasks/g0_bubble_sort.py
@@ -10,4 +10,14 @@ def sort(container: Collection[_Tt]) -> Collection[_Tt]:
 	:param container: container of elements to be sorted
 	:return: container sorted in ascending order
 	"""
+	j = 0
+	for k in range(len(container) - 1):
+		swapped = False
+		for i in range(len(container) - 1 - j):
+			if container[i] > container[i + 1]:
+				container[i + 1], container[i] = container[i], container[i + 1]
+				swapped = True
+		if not swapped:
+			break
+		j += 1
 	return container
